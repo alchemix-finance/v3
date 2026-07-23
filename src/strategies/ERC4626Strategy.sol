c80364af40eac05f55bb9072c53d5f7771867f45
d0m0l33 <dominic.lee766@gmail.com>
2026-07-23 09:18:14 -0400

    reverted ERC4626Strategy back to original state before merge, removed dupes

--- src/strategies/ERC4626Strategy.sol
+++ src/strategies/ERC4626Strategy.sol
@@ -16,7 +16,9 @@ contract ERC4626Strategy is MYTStrategy {
     bool public canForceDeallocate;
     event CanForceDeallocateUpdated(bool newCanForceDeallocate);
 
-    constructor(address _myt, StrategyParams memory _params, address _vault) MYTStrategy(_myt, _params) {
+    constructor(address _myt, StrategyParams memory _params, address _vault)
+        MYTStrategy(_myt, _params)
+    {
         mytAsset = IERC20(MYT.asset());
         vault = IERC4626(_vault);
         require(vault.asset() == MYT.asset(), "Vault asset != MYT asset");
@@ -29,7 +31,7 @@ contract ERC4626Strategy is MYTStrategy {
 
     function _allocate(uint256 amount) internal virtual override returns (uint256) {
         _ensureIdleBalance(address(mytAsset), amount);
-
+        
         TokenUtils.safeApprove(address(mytAsset), address(vault), amount);
         vault.deposit(amount, address(this));
         return amount;
@@ -69,9 +71,5 @@ contract ERC4626Strategy is MYTStrategy {
 
     function _isProtectedToken(address token) internal view virtual override returns (bool) {
         return token == MYT.asset() || token == address(vault);
-    }
-
-    function _canForceDeallocate() internal view virtual override returns (bool) {
-        return canForceDeallocate;
     }
 }
